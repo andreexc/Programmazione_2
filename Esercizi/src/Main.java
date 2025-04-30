e3434833a0a9de1d632d50dd66d7236ce37d1edd
andree__ <depaandrea05@gmail.com>
2025-05-01 00:05:10 +0200

    Esercizi Collections

---
+++ Esercizi/src/Main.java
@@ -0,0 +1,9 @@
+import Lez8_Collections.GiocoRamino;
+import Lez8_Collections.StampaDadi;
+
+public class Main {
+    public static void main(String[] args) {
+        //GiocoRamino.Gioca();
+        StampaDadi.StampaCombinazioniDadi();
+    }
+}
